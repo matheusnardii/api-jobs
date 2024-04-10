664ffc19256e08e5de3cea575c044ddd4411dd9d
matheusnardi <matheusp.nardi@gmail.com>
2024-04-10 17:53:21 +0100

    feat: adding applications services and controllers

--- src/services/application.services.ts
+++ src/services/application.services.ts
@@ -1,10 +1,17 @@
-export class ApplicationServices{
-    create(){
+import { prisma } from "../database/prisma";
+import { TApplication, TApplicationCreate } from "../schemas/application.schema";
 
+export class ApplicationServices {
+    async create(opportunityId: number, body: TApplicationCreate): Promise<TApplication> {
+        const data = await prisma.application.create({ data: { opportunityId, ...body } });
+
+        return data;
     }
 
-    readMany(){
-
+    async readMany(opportunityId: number): Promise<TApplication[]> {
+        const data = await prisma.application.findMany({where: {opportunityId}});
+        
+        return data;
     }
 
 }
